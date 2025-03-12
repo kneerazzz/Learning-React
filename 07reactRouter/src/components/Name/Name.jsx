17a8a65a36e17af43329dd21a55bf02b8e6375a4
kneerazzz <troyyig@gmail.com>
2025-03-12 20:45:18 +0530

    Use of react router

--- 07reactRouter/src/components/Name/Name.jsx
+++ 07reactRouter/src/components/Name/Name.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Name() {
+    const [name , setName] = useState('Neeraj')
+    setName(Troyy)
     return (
         <div className="flex justify-center items-center bg-red-500">
-            <div className="bg-red-500 text-black text-3xl p-4">Neeraj</div>
+            <div className="bg-red-500 text-black text-3xl p-4">{name}</div>
         </div>
     )
 }
